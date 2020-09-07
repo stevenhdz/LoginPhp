b57f9534c4bb2e5686b15280e0d2098333357f42
Alexander Jimenez <stevenhernandezj@gmail.com>
2020-09-07 12:32:34 -0500

    integraciones y separacion de vistas
    integraciones y separacion de vistas

---
+++ header.php
@@ -0,0 +1,3 @@
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="estilos.css" rel="stylesheet" type="text/css"/>
+        <link href="plugins/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css"/>
--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
-    <head>
+    <header>
         <title>Login</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="estilos.css" rel="stylesheet" type="text/css"/>
-        <link href="plugins/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css"/>
-    </head>
+        <?php
+        require 'header.php';
+        ?>
+    </header>
     <body>
 
         <div id="login">
@@ -39,10 +39,10 @@
 
         </div>
 
-        <script src="popper/popper.min.js" type="text/javascript"></script>
-        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="jquery/jquery-3.5.1.min.js" type="text/javascript"></script>
-        <script src="plugins/sweetalert2/sweetalert2.all.min.js" type="text/javascript"></script>
-        <script src="codigo.js" type="text/javascript"></script>
+        <footer>
+            <?php
+              require 'footer.php';
+            ?>
+        </footer>
     </body>
 </html>
